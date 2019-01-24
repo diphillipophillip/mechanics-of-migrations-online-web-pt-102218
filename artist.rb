1354b9836100316879ebbed2397ee81e51bf5b65
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-24 15:24:14 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 class Artist < ActiveRecord::Base
 end 
+=======
+class Artist < ActiveRecord::Base 
+ 
+end 
+ 
+>>>>>>> a7e9f103e31d0ea19ab4ada760ee36500d974d71
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,9 @@
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> a7e9f103e31d0ea19ab4ada760ee36500d974d71
 class CreateArtists < ActiveRecord::Migration[5.0] 
     def change 
         create_table :artists do |t| 
